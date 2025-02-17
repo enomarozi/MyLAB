910b7466d8c18068e510faef5a3c32bff66b3761
Eno Marozi <marozieno0@gmail.com>
2025-02-17 15:32:23 +0700

    17-02-2025

--- Cache-Poisoning/Python-Flask/main.py
+++ Cache-Poisoning/Python-Flask/main.py
@@ -1,32 +1,83 @@
-from flask import Flask, redirect, url_for, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, url_for, request,Response, render_template
 import uuid
+import mariadb
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://admin:password@localhost:3306/sqli?charset=utf8mb4&collation=utf8mb4_general_ci"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config["CACHE_TYPE"] = "RedisCache"
+app.config["CACHE_REDIS_HOST"] = "localhost"
+app.config["CACHE_DEFAULT_TIMEOUT"] = 604800
 
-class Content(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	uuid = db.Column(db.String(50), nullable=False)
+def connectionDB():
+	try:
+		conn = mariadb.connect(
+			host="127.0.0.1",
+			user="root",
+			password="password",
+			database="sqli",
+		)
 
-def normalize_uuid(uuid: str):
-	uuid_l = list(uuid)
-@app.route('/status/<status_>/')
-def status(status_):
-	return render_template('status.html', message=status_)
+		return conn
+	except mariadb.Error as e:
+		return {"Error":str(e)}
+
+def query(query, params=None):
+	conn = connectionDB()
+	if isinstance(conn, dict):
+		return conn
+
+	try:
+		cursor = conn.cursor(dictionary=True)
+		cursor.execute(query, params or ())
+		conn.commit()
+		result = {
+			"success":True,
+			"affected_rows":cursor.rowcount,
+			"result":cursor.fetchall(),
+		}
+
+		return result
+	except mariadb.Error as e:
+		print("Error:",e,flush=True)
+		return {"error":str(e)}
+	finally:
+		cursor.close()
+		conn.close()
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	print("Request cookies: ", request.cookies)
-	print("Request args: ", request.args)
 	if "id" not in request.cookies:
 		unique_id = str(uuid.uuid4())
-		print(unique_id)
+		query("INSERT INTO users VALUES(%s, %s);",(unique_id,0))
+	else:
+		unique_id = request.cookies.get("id")
+		res = query("SELECT * FROM users WHERE id=%s;",(unique_id,))
+		print(res,flush=True)
+		if "affected_rows" not in res:
+			print("Error:",res)
+			return "ERROR"
+		if res["affected_rows"] == 0:
+			unique_id = str(uuid.uuid4())
+			query("INSERT INTO users VALUES(%s, %s);",(unique_id,0))
+	html = f"""
+	<!DOCTYPE html>
+    <html>
+    <head>
+        <title>{unique_id}</title>
+    </head>
+    <body>
+        <h1>Your unique account ID: {unique_id}</h1>
+        <p><a href="/check?uuid={unique_id}">Click here to check if you are a winner!</a></p>
+    </body>
+    </html>
+	"""
+	r = Response(html)
+	r.set_cookie("id",unique_id)
+	return r
 
-	return f"Cookies: {request.cookies}, Request Args: {request.args}"
-
+@check_bp.route("/check")
+@cache.cached(timeout=604800, make_cache_key=make_cache_key)
+def check():
+	
 if __name__ == "__main__":
 	app.run(debug=True)
