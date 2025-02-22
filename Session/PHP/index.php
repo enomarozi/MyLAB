80dbb0aad386d87fc5290917eef26c6f62a48c98
root <root@Kali>
2025-02-22 14:00:39 +0700

    22-02-2025

---
+++ Session/PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sess.io</title>
+</head>
+<body>
+<p>Please provide the necessary data!</p>
+<h1>Sess.io</h1>
+<h3>Sign up</h3>
+<form method="POST">
+	<label for="username">Username:</label><br>
+	<input type="text" id='username' name="username"><br>
+	
+	<label for="password">Password:</label><br>
+	<input type="text" id='password' name="password"><br>
+	<button type="submit">Submit</button>
+</form>
+<p>To view the source code, <a href='/?source'>click here.</a></p>
+</body>
+</html>
